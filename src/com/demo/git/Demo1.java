0438d2ddf8357daaf56902b34ce40cccc8557432
aishwarya-bolegave <aishwaryabolegave@gmail.com>
2024-05-27 14:50:20 +0530

    second commit

---
+++ src/com/demo/git/Demo1.java
@@ -0,0 +1,26 @@
+package com.demo.git;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add("java");
+		a.add("python");
+		a.add(null);
+		a.add("php");
+		a.add("JDBC");
+		System.out.println(a);
+		Demo1.m1();
+		
+		
+
+	}
+
+	private static void m1() {
+		System.out.println("this is m1");
+		
+	}
+
+}
---
+++ src/com/demo/git/Demo2.java
@@ -0,0 +1,10 @@
+package com.demo.git;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("this is Dem02");
+
+	}
+
+}
