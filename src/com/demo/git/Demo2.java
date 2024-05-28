bc9e5e48e956817eead897744442f1c3e262d178
aishwarya-bolegave <aishwaryabolegave@gmail.com>
2024-05-28 12:38:25 +0530

    Update Demo2.java in main

--- src/com/demo/git/Demo2.java
+++ src/com/demo/git/Demo2.java
@@ -5,6 +5,9 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("this is Dem02");
 		System.out.println("this is Dem02");
+		for(int i=0;i<10;i++){
+		System.out.println(i);	
+		}
 
 	}
 
