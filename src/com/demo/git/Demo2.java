7b5664970bbc5e0aa07b4e6b18252cafa03e2876
aishwarya-bolegave <aishwaryabolegave@gmail.com>
2024-05-28 12:39:05 +0530

    done by dev2

--- src/com/demo/git/Demo2.java
+++ src/com/demo/git/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("this is Dem02");
 		System.out.println("this is Dem02");
+		System.out.print("====================");
 
 	}
 
